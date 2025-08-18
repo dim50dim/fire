776d4eb2e83eb120f188a4ee1f73c1ad9ed80fed
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-18 17:46:40 +0200

    added Navbar to App.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>
-         <h1>Navbar</h1>
+    <div className='flex justify-between bg-gray-200 w-full p-4'>
+              <h1 className='text-center text-2xl font-bold'>Firebase Google Auth &&& Context</h1>
+  
+       <Link to="/signin">Sign in</Link>
     </div>
   )
 }
