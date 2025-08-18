e8b8be67e9dc238ba203025af68d220a9ee909f8
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-18 19:00:52 +0200

    installed firebase.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className='flex justify-between bg-gray-200 w-full p-4'>
               <h1 className='text-center text-2xl font-bold'>Firebase Google Auth &&& Context</h1>
   
        <Link to="/signin">Sign in</Link>
+ 
     </div>
   )
 }
