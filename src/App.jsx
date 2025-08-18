bbed935ba152259d1059b44484e89a7b8e3cf367
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-18 19:20:25 +0200

    added AuthContextProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Navbar from './components/Navbar'
 import Signin from "./assets/pages/Signin";
 import Account from "./assets/pages/Account";
 import Home from "./assets/pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
  
 
   return (
     <>
-            <Navbar/>
+        <AuthContextProvider>
+    <Navbar/>
              <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/signin" element={<Signin/>}/>
                 <Route path="/account" element={<Account/>}/>
              </Routes>
+        </AuthContextProvider>
+        
     </>
   )
 }
