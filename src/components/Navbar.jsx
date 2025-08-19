3c1a36083819a9f8f002295e8d9d5fb2af3e44cf
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-19 16:46:01 +0200

    added one function to Navbar to logOut.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
-function Navbar() {
+
+ function Navbar() {
+  const {user,logOut} = UserAuth();
+  
+  const handleSignOut = async() => {
+  try {
+     await logOut()
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
   return (
   <div className="flex justify-between bg-gray-200 w-full p-4">
       <h1 className="text-center text-2xl font-bold">
               Firebase Google && Context
       </h1>
-      <Link to="/signin">Sign in</Link>
+        {user?.displayName ? <button onClick={handleSignOut}>Log out</button> : <Link to="/signin">Sign in</Link> }
+     
   </div>
   );
 }
