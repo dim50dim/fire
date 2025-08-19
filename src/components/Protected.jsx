49bf1af96e316b3a012915553726e9af134498ea
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-19 20:58:08 +0200

    Protected also was added some lines of code.

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
-const Protected = () => {
+
+const Protected = ({children}) => {
     const {user} = UserAuth();
+
     if(!user) {
         return <Navigate to="/"/>
     }
-  return (
-    <div>
-      
-    </div>
-  )
+    return children;
 }
 
 export default Protected
